463b77f966241107e3219ea9ed2f5aec4f2b742a
ken-rmndbn <remandabankenneth29@gmail.com>
2026-06-20 16:02:10 +0800

    file and code for package keeper

---
+++ smart_devices/__init__.py
@@ -0,0 +1,5 @@
+from .base_device import SmartDevice
+from .smart_light import SmartLight
+from .smart_thermostat import SmartThermostat
+
+__all__ = ["SmartDevice", "SmartLight", "SmartThermostat"]
